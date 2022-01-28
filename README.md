9fac88d1543907164d90235b47af78571a5fc96a
Pranay Agarwal <pranay12@connect.hku.hk>
2022-01-28 19:02:35 +0800

    Update readme and logging

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # wordle-ai
-An AI bot that attempts to solve the Wordle puzzle using letter-position heuristics (no answer lookups :P)
+
+A simple program that solves the Wordle puzzle by process of elimination. Currently, it uses up ~5 moves to correctly guess each word.
+
+## Usage
+
+Simply clone and run `npm run start` locally to start the program. After each guess, the script requires an input that validates its guess.
+
+'1' -> Correct letter in right position
+
+'0' -> Incorrect position but letter in word
+
+'-' -> Letter not in word (any character instead of 0 or 1 will satisfy this constraint)
+
+```
+--- The correct word is "happy"
+Engine guesses APPLE: -01--
+```
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ function main() {
   const engine = new WordGuessEngine();
   let result = "",
     numGuesses = 0;
-  while (result !== "11111") {
+  while (result !== "11111" || numGuesses >= 6) {
     numGuesses++;
     const guess = engine.getGuess();
     result = readline.question(`Engine guesses ${guess.toUpperCase()}: `);
@@ -13,8 +13,20 @@ function main() {
       throw new Error("SMALL RESULT");
     }
     engine.handleGuessResult(guess, result);
+    console.log(
+      "Reduced sample size to",
+      engine.filteredWordList.length,
+      "words\n"
+    );
   }
-  console.log("Correctly guessed in", numGuesses, "tries");
+  if (numGuesses <= 6) {
+    console.log("Correctly guessed in", numGuesses, "tries");
+  } else {
+    console.log(
+      "Could not guess the word correctly... it was",
+      WordGuessEngine.getCorrectGuessOfDay().toUpperCase
+    );
+  }
 }
 
 main();
