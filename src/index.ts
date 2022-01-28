478d745cc893867c8eaa04b971db522d8202b008
Pranay Agarwal <pranay12@connect.hku.hk>
2022-01-28 12:25:59 +0800

    Letter weightage and simple input

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+import * as readline from "readline-sync";
+import listOfWords from "./wordList";
+
+async function main() {
+  const answer = readline.question("Enter a word: ");
+  console.log(
+    `${answer} is ${listOfWords.includes(answer) ? "" : "not "}a valid word`
+  );
+}
+
+main();
